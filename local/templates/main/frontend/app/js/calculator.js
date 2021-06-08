85b3a6a7c32e7516fa75b0863fa3b45c8372f9de
ATMiura <leha.google@mail.ru>
2021-06-08 23:54:37 +0300

    Калькулятор

--- local/templates/main/frontend/app/js/calculator.js
+++ local/templates/main/frontend/app/js/calculator.js
@@ -13,21 +13,50 @@ export default class Calculator {
 
         let sum = 0;
 
-        $(self.calculatorForm).find('input:checked').each(function () {
+        $(self.calculatorForm).find('[name="delegate"]:checked').each(function () {
             sum += Number($(this).val()) || 0 // если value не корректно
         });
         $(self.calculatorForm).find('input:not([type="radio"])').each(function () {
             sum += Number($(this).val()) || 0 // если value не корректно
-        })
+        });
         $(self.calculatorResult).text(sum+ ' USD');
     }
 
     events() {
         let self = this;
 
-        $(document).on('input', this.calculatorInput, function() {
-            self.handler($(this));
+        //$(document).on('input', this.calculatorInput, function() {
+        //    self.handler($(this));
+        //});
+        //self.handler(this.calculatorInput);
+        $(document).on('input', this.calculatorInput, function (){
+            let sum = 0;
+            let investLabel = $('[data-invest-label]:checked').attr('data-invest-label');
+
+            if($('[name="delegate"]:checked').val() === 'yes'){
+                $('#mdc').attr('value', '0.13');
+                $('#usd').attr('value', '0.08');
+
+                let r = $('[name="invest"]:checked').val();
+                let t = $('[data-input-range]').val();
+                let P = $('#investSum').val();
+                sum = parseInt((P*(1 + r*t)));
+
+                $(self.calculatorResult).text(sum+' '+investLabel);
+
+            } else {
+                $('#mdc').attr('value', '0.1');
+                $('#usd').attr('value', '0.05');
+
+
+                let r = $('[name="invest"]:checked').val();
+                let t = $('[data-input-range]').val();
+                let P = $('#investSum').val();
+                sum = parseInt((P*(1 + r*t))) ;
+
+                $(self.calculatorResult).text(sum+' '+investLabel);
+
+            }
         });
-        self.handler(this.calculatorInput);
     }
 }
